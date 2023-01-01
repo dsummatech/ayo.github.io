3ad2e0e117c20ebf692a06be3eca517e491ddb03
dsummatech <jahtech1@gmail.com>
2023-01-01 20:55:26 +0100

    new site

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*  ---------------------------------------------------
+    Template Name: Male Fashion
+    Description: Male Fashion - ecommerce teplate
+    Author: Colorib
+    Author URI: https://www.colorib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+
+
+$(function () {
+$(".mini").click(function() {
+    $(".navbar").toggle("10 slow");
+});
+});
